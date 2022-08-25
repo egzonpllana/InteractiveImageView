1bc1f3aff388f69cdca54ed04f812b73dc5a3987
Egzon Pllana <docpllana@gmail.com>
2022-08-25 22:16:23 +0200

    Carthage - Add support for Carthage by adding .xcodeproj file.

---
+++ Carthage/Build/iOS/InteractiveImageView.framework/Sources/InteractiveImageView/Enums/IIVFocusOffset.swift
@@ -0,0 +1,14 @@
+//
+//  IIVFocusOffset.swift
+//  InteractiveImageView
+//
+//  Created by Egzon Pllana on 28.8.22.
+//  Copyright © 2022 Egzon Pllana. All rights reserved.
+//
+
+import Foundation
+
+public enum IIVFocusOffset: Int {
+    case begining
+    case center
+}
---
+++ Carthage/Build/iOS/InteractiveImageView.framework/Sources/InteractiveImageView/Helpers/IIVCropHandler.swift
@@ -0,0 +1,35 @@
+//
+//  IIVCropHandler.swift
+//  InteractiveImageView
+//
+//  Created by Egzon Pllana on 28.8.22.
+//  Copyright © 2022 Egzon Pllana. All rights reserved.
+//
+
+// Inspired by:
+// https://developer.apple.com/documentation/coregraphics/cgimage/1454683-cropping
+
+import UIKit
+
+public struct IIVCropHandler {
+    static func cropImage(_ inputImage: UIImage, toRect cropRect: CGRect, viewWidth: CGFloat, viewHeight: CGFloat) -> UIImage? {
+        let imageViewScale = max(inputImage.size.width / viewWidth,
+                                 inputImage.size.height / viewHeight)
+
+        // Scale cropRect to handle images larger than shown-on-screen size
+        let cropZone = CGRect(x:cropRect.origin.x * imageViewScale,
+                              y:cropRect.origin.y * imageViewScale,
+                              width:cropRect.size.width * imageViewScale,
+                              height:cropRect.size.height * imageViewScale)
+
+        // Perform cropping in Core Graphics
+        guard let cutImageRef: CGImage = inputImage.cgImage?.cropping(to:cropZone)
+        else {
+            return nil
+        }
+
+        // Return image to UIImage
+        let croppedImage: UIImage = UIImage(cgImage: cutImageRef)
+        return croppedImage
+    }
+}
