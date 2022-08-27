135ca910d497a86be9099a5437967d36333f9b6c
Egzon Pllana <docpllana@gmail.com>
2022-08-27 17:27:31 +0200

    Add example project.

---
+++ InteractiveImageViewExample/InteractiveImageViewExample/ViewControllers/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  InteractiveImageViewExample
+//
+//  Created by Egzon Pllana on 25.8.22.
+//
+
+import UIKit
+import InteractiveImageView
+
+class ViewController: UIViewController {
+
+    // MARK: - Outlets
+
+    @IBOutlet private weak var imageView: InteractiveImageView!
+    @IBOutlet private weak var croppedImageView: UIImageView!
+
+    // MARK: - View life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        // Configure InteractiveImageView
+        configureImageView()
+    }
+
+    // MARK: - Methods
+
+    private func configureImageView() {
+        let imageExample = #imageLiteral(resourceName: "image.png")
+        imageView.configure(withNextContentMode: .customOffset(offset: Double(2)/Double(3)), withFocusOffset: .center, withImage: imageExample)
+        imageView.interactiveImageViewDelegate = self
+    }
+
+    // MARK: - Actions
+
+
+    @IBAction func changeContentModePressed(_ sender: Any) {
+        imageView.toggleImageContentMode()
+    }
+
+    @IBAction func cropImagePressed(_ sender: Any) {
+        imageView.cropImage()
+    }
+}
+
+extension ViewController: InteractiveImageViewDelegate {
+    func didCropImage(image: UIImage) {
+        self.croppedImageView.image = image
+    }
+
+    func didFailImageCropping() {
+        //
+    }
+
+    func didFailTogglingContentMode() {
+        //
+    }
+
+    func didFailAdjustingFramesWhenZooming() {
+        //
+    }
+
+    func didFailToGetImageView() {
+        //
+    }
+}
