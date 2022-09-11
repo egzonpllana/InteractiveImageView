236107b72430004cab2a972cc772363e597b13e4
Egzon Pllana <docpllana@gmail.com>
2022-09-11 14:13:58 +0200

    Added new enum: FailType.

---
+++ Sources/InteractiveImageView/Enums/IIVFailType.swift
@@ -0,0 +1,16 @@
+//
+//  IIVFailType.swift
+//  InteractiveImageView
+//
+//  Created by Egzon Pllana on 11.09.22.
+//  Copyright © 2022 Egzon Pllana. All rights reserved.
+//
+
+import Foundation
+
+public enum IIVFailType {
+    case imageCropping
+    case togglingContentMode
+    case adjustingFramesWhenZooming
+    case toGetImageView
+}
