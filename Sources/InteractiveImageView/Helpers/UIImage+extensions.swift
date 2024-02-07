e9bd710e6e2e73b10664b031759f0c4de83d1d4a
Egzon Pllana <>
2024-02-07 15:08:24 +0100

    Add new UIImage extension: rotatedBy().

---
+++ Sources/InteractiveImageView/Helpers/UIImage+extensions.swift
@@ -0,0 +1,26 @@
+//
+//  UIImage+extensions.swift
+//  InteractiveImageView
+//
+//  Created by Egzon Pllana on 7.2.24.
+//  Copyright © 2024 Egzon Pllana. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Rotate image -
+extension UIImage {
+    func rotated(by degrees: CGFloat) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+
+        context.translateBy(x: self.size.width / 2, y: self.size.height / 2)
+        context.rotate(by: degrees * CGFloat.pi / 180.0)
+
+        self.draw(at: CGPoint(x: -self.size.width / 2, y: -self.size.height / 2))
+        let rotatedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return rotatedImage
+    }
+}
